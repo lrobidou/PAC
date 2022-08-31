104296a4b6ba108c56a0263546dd2d930839432b
Lucas Robidou <lucas.robidou@inria.fr>
2022-08-31 17:40:13 +0200

    coverage is an option

--- install.sh
+++ install.sh
@@ -9,18 +9,21 @@ function project_build ()
 
   cmake .. -DCMAKE_BUILD_TYPE=${1} \
            -DWITH_TESTS=${2} \
+           -DWITH_COVERAGE=${2} \
            -DSTATIC_BUILD=${3} \
            -DDEV_BUILD="${4}"
   make -j${5}
-
-  if [[ ${6} == 1 && $? == 0 ]]; then
+  alrighty=$(($? == 0))  # is the return code of make 0 ?
+  if [[ ${1} == "Coverage" && ${alrighty} == 1 ]]; then
+    make coverage-report
+  elif [[ ${6} == 1 && ${alrighty} == 1 ]]; then
     ctest --verbose
   fi
 }
 
 function usage ()
 {
-  echo "PAC build script."
+  echo "fimpera build script."
   echo "Usage: "
   echo "  ./install.sh [-r str] [-t int] [-j int] [-d] [-s]"
   echo "Options: "
@@ -35,6 +38,7 @@ function usage ()
 
 rel="Release"
 deb="Debug"
+cov="Coverage"
 
 mode="Release"
 dev="OFF"
@@ -49,7 +53,7 @@ while getopts "r:k:t:c:j:onwmsh" option; do
   case "$option" in
     r)
       mode=${OPTARG}
-      [[ ${mode} != ${rel} && ${mode} != ${deb} ]] && usage
+      [[ ${mode} != ${rel} && ${mode} != ${deb} && ${mode} != ${cov} ]] && usage
       ;;
     t)
       tests=${OPTARG}
